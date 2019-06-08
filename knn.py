56faba7fe8a668882f362816e212fa05cb614bc9
guyraz89 <39309726+guyraz89@users.noreply.github.com>
2019-06-08 16:08:51 +0300

    Update knn.py

--- knn.py
+++ knn.py
@@ -3,6 +3,7 @@ from sklearn import datasets
 import operator
 import csv
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 
 def loadData():
@@ -14,6 +15,7 @@ def loadData():
 
 '''
     --euclideanDistance--
+
     Input: 
         inst1 - vector of size 
         inst2 - vector of size
@@ -31,6 +33,7 @@ def euclideanDistance(inst1, inst2, length):
 
 '''
     --getNeighbors--
+
     Input: 
         TrainingSet - List of samples of the training set (with the tagg collum (y)).
         testInstance - chosen instance to be look at.
@@ -53,28 +56,33 @@ def getNeighbors(trainingSet, testInstance, k):
 
 
 '''
-    --getResponse--
+    --getDecision--
+
     Input: 
         neighbors - neighbors of some instance
+
     Output:
         returns the id of the most common class of all neighbors.
 '''
-def getResponse(neighbors):
+def getDecision(neighbors):
 	classVotes = {}
 	for x in range(len(neighbors)):
-		response = neighbors[x][-1]
-		if response in classVotes:
-			classVotes[response] += 1
+		decision = neighbors[x][-1]
+		if decision in classVotes:
+			classVotes[decision] += 1
 		else:
-			classVotes[response] = 1
+			classVotes[decision] = 1
 
 	return max(classVotes.items(), key=operator.itemgetter(1))[0]
 
+
 '''
     --getAccuracy--
+
     Input:
         testSet - instances(with tags) ([[1,2,3,0], [1,4,5,0]]).
         predictions - predictions for the given testSet by KNN ([0, 0]).
+
     Output:
         returns the prediction accuracy in percents (100%).
 ''' 
@@ -85,22 +93,31 @@ def getAccuracy(X, predictions):
 			correct += 1
 	return (correct/float(len(X))) * 100.0
 
+'''
+    --KNN--
 
+    Input:
+        X - training set with the y appended as the last collum ( [1, 2, 3, 'a'])
+        k - k of the algorithm (how many neighbors to search for each instance).
+
+    Output:
+        accuracy - percents of right predictions.
+        predictions - the predictions for each sample.
+'''
 def KNN(X, k):
     predictions = []
     for x in range(len(X)):
         neighbors = getNeighbors(X, X[x], k)
-        result = getResponse(neighbors)
+        result = getDecision(neighbors)
         predictions.append(result)
         print('predicted=' + repr(result) + ', actual=' + repr(X[x][-1]))
     accuracy = getAccuracy(X, predictions)
     print('Accuracy: ' + repr(accuracy) + '%')
-
+    return accuracy, predictions
 
 
 if __name__ == '__main__':
-    k = 2
-    testInstance = [6.7, 3.0, 5.2, 2.0]
+    k = 5
     X, y = loadData()
-    X = np.concatenate((X,y[:,None]),axis=1)
-    KNN(X, k)
+    X = np.concatenate((X, y[:,None]), axis=1)
+    acc, pred = KNN(X, k)
